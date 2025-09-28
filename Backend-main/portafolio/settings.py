ed9dfca3df918bc634a62c1cc1a57dd45d37c93d
syur <bp113534@email.com>
2025-09-28 23:55:30 +0200

    subida de permisos en host

--- Backend-main/portafolio/settings.py
+++ Backend-main/portafolio/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = True
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://portfolio-pijl.onrender.com"]
 
 # Configuración de correo con Gmail
 EMAIL_HOST = config("EMAIL_HOST")
